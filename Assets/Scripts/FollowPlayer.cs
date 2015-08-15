e01066c6cb3bb7ea66c55126af700f556ac28841
patriotpie <patriotpie@gmail.com>
2015-08-15 10:56:44 -0400

    First commit - most basic mechanics

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	
+	public float dampTime = 0.1f;
+	private Vector3 velocity = Vector3.zero;
+	public GameObject target;
+	private Transform targetTransform;
+	private Camera camera;
+
+	void Start()
+	{
+		camera = this.gameObject.GetComponent<Camera> ();
+		targetTransform = target.transform;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (targetTransform)
+		{
+			Vector3 point = camera.WorldToViewportPoint(targetTransform.position);
+			Vector3 delta = targetTransform.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			Vector3 destination = transform.position + delta;
+			// smooth:
+			//transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			// directly follow:
+			transform.position = destination;
+		}
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	private float xVel;
+	private float yVel;
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		xVel = 0.0f;
+		yVel = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		xVel += Input.GetAxis ("Horizontal") * Speed;
+		yVel += Input.GetAxis ("Vertical") * Speed;
+
+		transform.position += Time.deltaTime * new Vector3 (xVel, yVel, 0.0f);
+		
+	}
+}
