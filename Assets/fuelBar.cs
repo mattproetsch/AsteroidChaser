03043fdb51a057d4fe71d39c5697b5fa51bd9297
Segger <saintdane@gmail.com>
2015-08-15 15:28:35 -0400

    Fonts nd HUD stuff

---
+++ Assets/fuelBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fuelBar : MonoBehaviour {
+
+	private RectTransform _rXform;
+	private float _barScale;
+
+	// Use this for initialization
+	void Start () {
+		_rXform = this.GetComponent<RectTransform> ();
+		_barScale = _rXform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//_barScale = ; //Get ship fuel level
+		_rXform.localScale = new Vector3 (_barScale, _rXform.localScale.y);
+	}
+}
