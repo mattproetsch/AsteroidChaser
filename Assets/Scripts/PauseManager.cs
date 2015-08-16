4c12086b5a76be1538a9658c3437941910809259
John Roth <johnroth321@gmail.com>
2015-08-16 16:41:19 -0400

    intro scene commit
    pls gooby

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+	private bool paused;
+	public GameObject Menus;
+	public GameObject Menu;
+	// Use this for initialization
+	void Awake () {
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (paused) {
+			Vector2 tempVector = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis ("Vertical"));
+			float tempAngle = Mathf.Rad2Deg*Mathf.Atan (tempVector.y/tempVector.x);
+			Debug.Log (tempAngle);
+			if(tempAngle<30f&&tempAngle>-30f&&tempVector.x>0f){
+				GameObject.Find ("Exit").GetComponent<Image>().color=new Color (176f/255f,184f/255f,45f/255f,1f);
+			}
+			else{
+				GameObject.Find ("Exit").GetComponent<Image>().color=new Color (109f/255f,28f/255f,108f/255f,1f);
+			}
+			if(tempAngle<90f&&tempAngle>30f&&tempVector.x>0f){
+				GameObject.Find ("Options").GetComponent<Image>().color=new Color (176f/255f,184f/255f,45f/255f,1f);
+			}
+			else{
+				GameObject.Find ("Options").GetComponent<Image>().color=new Color (109f/255f,28f/255f,108f/255f,1f);
+			}
+			if(tempAngle<-30f&&tempAngle>-90f&&tempVector.x>0f){
+				GameObject.Find ("Load").GetComponent<Image>().color=new Color (176f/255f,184f/255f,45f/255f,1f);
+			}
+			else{
+				GameObject.Find ("Load").GetComponent<Image>().color=new Color (109f/255f,28f/255f,108f/255f,1f);
+			}
+
+
+
+			if(tempAngle<30f&&tempAngle>-30f&&tempVector.x<0f){
+				GameObject.Find ("Map").GetComponent<Image>().color=new Color (176f/255f,184f/255f,45f/255f,1f);
+			}
+			else{
+				GameObject.Find ("Map").GetComponent<Image>().color=new Color (109f/255f,28f/255f,108f/255f,1f);
+			}
+			if(tempAngle<90f&&tempAngle>30f&&tempVector.x<0f){
+				GameObject.Find ("Save").GetComponent<Image>().color=new Color (176f/255f,184f/255f,45f/255f,1f);
+			}
+			else{
+				GameObject.Find ("Save").GetComponent<Image>().color=new Color (109f/255f,28f/255f,108f/255f,1f);
+			}
+			if(tempAngle<-30f&&tempAngle>-90f&&tempVector.x<0f){
+				GameObject.Find ("Inventory").GetComponent<Image>().color=new Color (176f/255f,184f/255f,45f/255f,1f);
+			}
+			else{
+				GameObject.Find ("Inventory").GetComponent<Image>().color=new Color (109f/255f,28f/255f,108f/255f,1f);
+			}
+
+
+
+
+
+
+
+			if(Input.GetButtonDown ("Fire1")){
+				if(Menu.activeInHierarchy){Menu.SetActive(false);}
+				else{Menu.SetActive(true);}
+			}
+		}
+		if (Input.GetButtonDown ("Fire3") && !paused) {
+			Debug.Log ("skdljfsdkljsfjslk;dajf;l");
+			Time.timeScale = 0.0f;
+			paused=true;
+			Menus.SetActive(true);
+			return;
+		} 
+		if (Input.GetButtonDown ("Fire3") && paused) {
+			Debug.Log ("skdljfsdkljsfjslk;dajf;l");
+			Time.timeScale = 1;
+			paused = false;
+			Menus.SetActive (false);
+			return;
+		}
+
+		//Debug.Log(System.Convert.ToString(Input.GetJoystickName));
+	}
+}
---
+++ Assets/WelcomeScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WelcomeScreen : MonoBehaviour {
+	private int curstep;
+	private int counter;
+	// Use this for initialization
+	void Start () {
+		curstep = 1;
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown&&curstep==1) {
+			curstep=2;
+		}
+		if (curstep == 2) {
+			Debug.Log(counter);
+			GameObject.Find ("ScrollText").GetComponent<RectTransform>().position=new Vector3(GameObject.Find ("ScrollText").GetComponent<RectTransform>().position.x,GameObject.Find ("ScrollText").GetComponent<RectTransform>().position.y+1,GameObject.Find ("ScrollText").GetComponent<RectTransform>().position.z);
+			counter++;
+			Debug.Log(counter);
+			if(counter>100000000000000000){curstep=3;}
+			Debug.Log(counter);
+		}
+		if (Input.anyKeyDown && curstep == 2) {
+			curstep=3;
+		}
+		if (curstep == 3) {
+			Application.LoadLevel("mainscene");
+		}
+	}
+}
