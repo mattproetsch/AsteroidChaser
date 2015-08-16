620d69bb349f1c8300ba7d62d13be72cf5ca8ea5
patriotpie <patriotpie@gmail.com>
2015-08-16 12:14:26 -0400

    Asteroids + Prefab objs

---
+++ Assets/SpawnAsteroids.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnAsteroids : MonoBehaviour {
+
+	public GameObject Asteroid1;
+	public GameObject Asteroid2;
+	public GameObject Asteroid3;
+	public GameObject Asteroid4;
+	public GameObject Asteroid5;
+
+	public int XSpacing;
+	public int YSpacing;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
