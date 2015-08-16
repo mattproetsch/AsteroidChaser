e6f4fd6c286a06079bb673b81df7301ed59ef277
patriotpie <patriotpie@gmail.com>
2015-08-16 11:21:38 -0400

    This is the tiling system you get when you have 1hr

--- Assets/CreateTiles.cs
+++ Assets/CreateTiles.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CreateTiles : MonoBehaviour {
 
-	public Sprite Star;
-	public Sprite Planet;
-	public Sprite Galaxy;
-	public Sprite Satellite;
-	public Sprite StarSystem;
+	public GameObject Star;
+	public GameObject Planet;
+	public GameObject Galaxy;
+	public GameObject Satellite;
+	public GameObject StarSystem;
 
 	private Camera _mainCamera;
 	private GameObject _playerShip;
@@ -16,30 +17,113 @@ public class CreateTiles : MonoBehaviour {
 	private Vector2 _viewportSize;
 	private Vector2 _tilesCenter;
 
-	private Sprite[,] _sprites;
+	private List<GameObject> _sprites;
 
 	void Start() {
 		_mainCamera = Camera.main;
 		_playerShip = GameObject.Find ("Ship");
 		_move = _playerShip.GetComponent<Move> ();
 
+		_sprites = new List<GameObject> ();
 		UpdateTileSize ();
 		GenerateSprites ();
+		PositionSpritesInitial ();
 	}
 
 	void Update() {
-		if (NeedToGenerate ()) {
-			GenerateSprites ();
+		if (NeedToRepositionSprites ()) {
+			RepositionSprites ();
 		}
 	}
 
-	bool NeedToGenerate() {
-		return true;
+	bool NeedToRepositionSprites() {
+		Vector3 shipPos = _playerShip.transform.position;
+		return shipPos.x < _tilesCenter.x - _viewportSize.x ||
+				shipPos.x > _tilesCenter.x + _viewportSize.x ||
+				shipPos.y < _tilesCenter.y - _viewportSize.y || 
+				shipPos.y > _tilesCenter.y + _viewportSize.y;
 	}
 
 	void GenerateSprites () {
-
 		_tilesCenter = _playerShip.transform.position;
+		Rect viewRect = GetCameraGameBounds ();
+
+		for (int i = 0; i < 500; i++) {
+			GameObject sprite = GameObject.Instantiate(Star) as GameObject;
+			_sprites.Add (sprite);
+		}
+
+		Debug.Log ("Generated " + _sprites.Count + " sprites");
+	}
+
+	void RepositionSprites() {
+		Debug.Log ("Repositioning sprites:");
+		Vector3 shipPos = _playerShip.transform.position;
+
+		if (shipPos.x < _tilesCenter.x - _viewportSize.x) {
+			// Reposition sprites to the right of shipPos.x + _viewportWidth/2
+			Debug.Log ("     Because ship moved left");
+			List<GameObject> repositionUs = _sprites.FindAll(sprite => sprite.transform.position.x > shipPos.x + _viewportSize.x);
+			Debug.Log (repositionUs.Count + " sprites to be moved");
+			float xMin = shipPos.x - 1.5f * _viewportSize.x;
+			float xMax = shipPos.x - 0.5f * _viewportSize.x;
+			Debug.Log ("Reposition sprites with x in [" + xMin + ", " + xMax + "]");
+			foreach (GameObject go in repositionUs) {
+				go.transform.position = new Vector3(Random.Range (xMin, xMax), go.transform.position.y, Random.Range(10, 20));
+			}
+			_tilesCenter.x = shipPos.x;
+		}
+		else if (shipPos.x > _tilesCenter.x + _viewportSize.x) {
+			Debug.Log ("     Because ship moved right");
+			// Reposition sprites to the left of shipPos.x - _viewportWidth/2
+			List<GameObject> repositionUs = _sprites.FindAll(sprite => sprite.transform.position.x < shipPos.x - _viewportSize.x);
+			Debug.Log (repositionUs.Count + " sprites to be moved");
+			float xMin = shipPos.x + 0.5f * _viewportSize.x;
+			float xMax = shipPos.x + 1.5f * _viewportSize.x;
+			Debug.Log ("Reposition sprites with x in [" + xMin + ", " + xMax + "]");
+			foreach (GameObject go in repositionUs) {
+				go.transform.position = new Vector3(Random.Range (xMin, xMax), go.transform.position.y, Random.Range (10, 20));
+			}
+			_tilesCenter.x = shipPos.x;
+		}
+
+		if (shipPos.y > _tilesCenter.y + _viewportSize.y) {
+			Debug.Log ("     Because ship moved up");
+			// REposition sprites below _shipPos.y - _viewportWidth/2
+			List<GameObject> repositionUs = _sprites.FindAll(sprite => sprite.transform.position.y < shipPos.y - _viewportSize.y);
+			Debug.Log (repositionUs.Count + " sprites to be moved");
+			float yMin = shipPos.y + 0.5f * _viewportSize.y;
+			float yMax = shipPos.y + 1.5f * _viewportSize.y;
+			Debug.Log ("Reposition sprites with y in [" + yMin + ", " + yMax + "]");
+			foreach (GameObject go in repositionUs) {
+				go.transform.position = new Vector3(go.transform.position.x, Random.Range (yMin, yMax), Random.Range (10, 20));
+			}
+			_tilesCenter.y = shipPos.y;
+		}
+		else if (shipPos.y < _tilesCenter.y - _viewportSize.y) {
+			Debug.Log ("     Because ship moved down");
+			// REposition sprites above _shipPos.y + _iweportWidth/2
+			List<GameObject> repositionUs = _sprites.FindAll(sprite => sprite.transform.position.y > shipPos.y + _viewportSize.y);
+			Debug.Log (repositionUs.Count + " sprites to be moved");
+			float yMin = shipPos.y - 1.5f * _viewportSize.y;
+			float yMax = shipPos.y - 0.5f * _viewportSize.y;
+			Debug.Log ("Reposition sprites with y in [" + yMin + ", " + yMax + "]");
+			foreach (GameObject go in repositionUs) {
+				go.transform.position = new Vector3(go.transform.position.x, Random.Range (yMin, yMax), Random.Range (10, 20));
+			}
+			_tilesCenter.y = shipPos.y;
+		}
+	}
+
+	void PositionSpritesInitial() {
+		Vector3 shipPos = _playerShip.transform.position;
+		float yMin = shipPos.y - 1.5f * _viewportSize.y;
+		float yMax = shipPos.y + 1.5f * _viewportSize.y;
+		float xMin = shipPos.x - 1.5f * _viewportSize.x;
+		float xMax = shipPos.x + 1.5f * _viewportSize.x;
+		for (int i = 0; i < 500; i++) {
+			_sprites [i].transform.position = new Vector3 (Random.Range (xMin, xMax), Random.Range (xMin, xMax), Random.Range (10, 20));
+		}
 	}
 
 	Rect GetCameraGameBounds() {
@@ -62,6 +146,6 @@ public class CreateTiles : MonoBehaviour {
 	void ViewportChanged()
 	{
 		UpdateTileSize ();
-		GenerateSprites ();
+		RepositionSprites ();
 	}
 }
