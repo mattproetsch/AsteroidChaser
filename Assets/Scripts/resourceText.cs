7c4c642172869573fc446e6e5a28c09abb58a19e
Segger <saintdane@gmail.com>
2015-08-16 15:20:06 -0400

    Resource Counter

---
+++ Assets/Scripts/resourceText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+
+public class resourceText : MonoBehaviour {
+	private Inventory inventory;
+	private GameObject ship;
+	public Text text;
+	private float dispAmount;
+	private int collected;
+	private float deltaAmount;
+
+	// Use this for initialization
+	void Start () {
+		ship = GameObject.Find ("Ship");
+		inventory = ship.GetComponent<Inventory> ();
+		text.text = "Resources: "+inventory.MineralXAmt.ToString();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		collected = inventory.MineralXAmt;
+		dispAmount += (collected-dispAmount)*0.05f;
+		text.text = "Resources: "+Mathf.Ceil (dispAmount).ToString();
+	}
+}
