d92963b78e07d54e3f468d846ea764833aaf0f87
Segger <saintdane@gmail.com>
2015-08-15 22:15:33 -0400

    Scantastrophe

--- Assets/Scripts/hudScanner.cs
+++ Assets/Scripts/hudScanner.cs
@@ -2,14 +2,40 @@
 using System.Collections;
 
 public class hudScanner : MonoBehaviour {
+	private GameObject _ship;
+	private shipScanner _scanner;
+	public GameObject BlipPrefab;
+	private GameObject _guiCanvas;
 
+	
 	// Use this for initialization
 	void Start () {
-	
+
+		_ship = GameObject.Find ("Ship");
+		_scanner = _ship.GetComponent<shipScanner> ();
+		_guiCanvas = GameObject.Find ("Canvas");
+
+		//Creates pool of blips for rendering
+		for (int i=0; i<15; i++) {
+			_scanner._blips.Add( (GameObject.Instantiate(BlipPrefab)) as GameObject );
+			_scanner._blips[i].transform.parent = _guiCanvas.transform;
+		}
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		for(int i=0;i<_scanner._scannedObjects.Count;i++) {
+			GameObject body = _scanner._scannedObjects[i];
+			GameObject blip = _scanner._blips[i];  
+
+			float dist = Vector3.Distance(body.transform.position,_ship.transform.position);
+			dist /= 200;
+			RectTransform rTransform = blip.GetComponent<RectTransform>();
+			rTransform.position = new Vector3(100,100,0);
+
+
+		}
 	}
 }
--- Assets/Scripts/shipScanner.cs
+++ Assets/Scripts/shipScanner.cs
@@ -6,42 +6,26 @@ using System.Collections.Generic;
 public class shipScanner : MonoBehaviour {
 	private GameObject _ship;
 	public List<GameObject> _scannedObjects;
-	private List<GameObject> _blips;
-
-	public GameObject BlipPrefab;
+	public List<GameObject> _blips;
+	
 
 	// Use this for initialization
 	void Start () {
-		_ship = GameObject.Find ("Ship");
 		_scannedObjects = new List<GameObject> ();
-
-		//Creates pool of blips for rendering
-		for (int i=0; i<=15; i++) {
-			_scannedObjects[i] = GameObject.Instantiate(BlipPrefab) as GameObject;
-		}
 
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
+	void OnTriggerEnter2D(Collider2D other) {
 		_scannedObjects.Add (other.gameObject);
 	}
 	
-	void OnTriggerExit2D(Collider2D other)
-	{
+	void OnTriggerExit2D(Collider2D other) {
 		_scannedObjects.Remove (other.gameObject);
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		foreach (GameObject scanned in _scannedObjects) {
-			float dist = Vector3.Distance(scanned.transform.position,_ship.transform.position);
-			dist /= 200;
-
-
-		}
-
 
 	}
 }
