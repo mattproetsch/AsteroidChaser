1c417578459408980881bcb2fbd6b65bd7477ef2
Segger <saintdane@gmail.com>
2015-08-16 15:57:35 -0400

    Landing text

---
+++ Assets/Scripts/landingText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+
+public class landingText : MonoBehaviour {
+	public Text text;
+	private GameObject BG;
+	private bool show;
+	
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Resources: "+inventory.MineralXAmt.ToString();
+		BG = GameObject.Find ("landingTextBG");
+	}
+
+	void showText(string s){
+		text.text = "Press B to land on " + s;
+		text.gameObject.SetActive (true);
+		return true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
